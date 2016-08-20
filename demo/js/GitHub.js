4d7adeef4256191c98f05239b9066ec01b606d01
kiricon <kiricon@live.com>
2016-08-19 21:43:16 -0600

    Added the ability to get stats from the last year.

--- demo/js/GitHub.js
+++ demo/js/GitHub.js
@@ -2,8 +2,10 @@
 function GitHub(){
 	this.data = {};
 	this.commits = [];
+	this.pulls = [];
 	this.user = "";
 	this.repos = [];
+	this.stats = 0;
 }
 
 GitHub.prototype.getCommits= function(repo, callback){
@@ -19,6 +21,34 @@ GitHub.prototype.getCommits= function(repo, callback){
 	});
 	
 }
+GitHub.prototype.getPulls= function(repo, callback){
+	var self = this;
+	var user = this.user;
+	jsonp("https://api.github.com/repos/"+user+"/"+repo+"/pulls?per_page=1000000&callback=json_callback", function(response){
+		console.log(response.data);
+		response.data.forEach(function(value){
+			console.log(value);
+			self.pulls.push(value.pull);
+		});
+		return callback();
+	});
+	
+}
+
+GitHub.prototype.getStats= function(repo, callback){
+	var self = this;
+	var user = this.user;
+	jsonp("https://api.github.com/repos/"+user+"/"+repo+"/stats/commit_activity?per_page=1000000&callback=json_callback", function(response){
+		//console.log(response);
+		response.data.forEach(function(value){
+			self.stats += value.total;
+		});
+		return callback();
+	});
+	
+}
+
+
 
 GitHub.prototype.getRepos = function(callback){
 	var self = this;
@@ -32,7 +62,7 @@ GitHub.prototype.getRepos = function(callback){
 	});
 }
 
-GitHub.prototype.getAllCommits = function() {
+GitHub.prototype.getAllCommits = function(callback) {
 	var self = this;
 	this.getRepos(function(){
 		var count = self.repos.length;
@@ -40,10 +70,44 @@ GitHub.prototype.getAllCommits = function() {
 			self.getCommits(value, function(){
 				count--;
 				if(count == 0){
-					alert('done');
-					console.log(self.commits);
+					callback();
 				}
 			});
 		});
 	});
 };
+
+GitHub.prototype.getAllPulls = function(callback) {
+	var self = this;
+	this.getRepos(function(){
+		var count = self.repos.length;
+		self.repos.forEach(function(value){
+			self.getPulls(value, function(){
+				count--;
+				if(count == 0){
+					callback();
+				}
+			});
+		});
+	});
+};
+
+GitHub.prototype.getAllStats = function(callback) {
+	var self = this;
+	this.getRepos(function(){
+		var count = self.repos.length;
+		self.repos.forEach(function(value){
+			self.getStats(value, function(){
+				count--;
+				if(count == 0){
+					callback();
+				}
+			});
+		});
+	});
+};
+
+
+
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 var github = new GitHub();
-github.user = "kiricon";
-github.getAllCommits();
+github.user = "Ellesent";
+/*
+github.getAllCommits(function(){
+	console.log(github.commits.length);
+	github.getAllPulls(function(){
+		console.log(github.pulls.length);
+	});
+}); */
+
+github.getAllStats(function(){
+	console.log(github.stats);
+});
